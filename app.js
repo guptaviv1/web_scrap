0f497792e660d2a56515c43d059cf606523f5cdc
Vivek Kumar <viv1g3pta@gmail.com>
2022-05-26 11:42:12 +0530

    Change the url and added updated tag elements for new url

--- app.js
+++ app.js
@@ -13,22 +13,22 @@ function webScrap (pagesToScrape) {
             }
             const browser = await puppeteer.launch();
             const page = await browser.newPage();
-            await page.goto("https://www.naukri.com/nodejs-jobs", { waitUntil: 'networkidle2',timeout: 0});
-            await page.waitForSelector('.listContainer');
+            await page.goto("https://www.monsterindia.com/search/node-js-jobs", { waitUntil: 'networkidle2',timeout: 0});
+            await page.waitForSelector('#srp-jobList');
             let currentPage = 1;
             let urls = [];
             while (currentPage <= pagesToScrape) {
                 let newUrls = await page.evaluate(() => {
                     let results = [];
-                    let items = document.querySelectorAll('article.jobTuple.bgWhite.br4.mb-8');
+                    let items = document.querySelectorAll('#srp-jobList .job-apply-card');
                     items.forEach((item) => {
                         results.push({
-                            job_title:  item.querySelector('a.title').innerText,
-                            company_name: item.querySelector('a.subTitle').innerText,
-                            location: item.querySelector('.location span').innerText,
-                            years_of_exp_req: item.querySelector('.experience span').innerText,
-                            salary: item.querySelector('.salary span').innerText,
-                            post_time: item.querySelectorAll('.jobTupleFooter .type span')[1].innerText
+                            job_title:  item.querySelector('.job-tittle h3 a').innerText,
+                            company_name: item.querySelector('.company-name a').innerText,
+                            location: item.querySelectorAll('.searctag .loc')[0].innerText,
+                            years_of_exp_req:  item.querySelectorAll('.searctag .loc')[1].innerText,
+                            salary:  item.querySelectorAll('.searctag .loc')[2].innerText,
+                            post_time: item.querySelector('.posted-update .posted').innerText
                         });
                     });
                     return results;
@@ -36,10 +36,11 @@ function webScrap (pagesToScrape) {
                 urls = urls.concat(newUrls);
                 if (currentPage < pagesToScrape) {
                     await Promise.all([
-                        await page.click('.pagination .fright.fs14.btn-secondary.br2'),
-                        await page.waitForSelector('article.jobTuple.bgWhite.br4.mb-8')
+                        await page.click('button.btn-next'),
+                        await page.waitForSelector('#srp-jobList')
                     ])
                 }
+                console.log("currentPage", currentPage);
                 currentPage++;
             }
             browser.close();
@@ -49,7 +50,7 @@ function webScrap (pagesToScrape) {
         }
     })
 }
-webScrap(5).then(console.log).catch(console.error);
+webScrap(2).then(console.log).catch(console.error);
 
 
 app.listen(port, () => {
